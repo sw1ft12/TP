b530e23d0f3dda5ca9bda687f7f1c863ecb0255c
sw1ft12 <potapov.da@phystech.edu>
2022-05-07 19:27:12 +0300

    Update run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+ 
 while [ -n "$1" ]
 do
-case "$1" in
+case "$1" in 
 --input_folder) input_folder="$2";;
 --extension) extension="$2";;
 --backup_folder) backup_folder="$2";; 
 --backup_archive_name) backup_archive_name="$2";;
---) shift
-break;;
+*) echo invalid option: $1
+ exit 1;;
 esac
-shift
+shift 2
 done
+if [ -z ${input_folder} ] || [ -z ${extension} ] || [ -z ${backup_folder} ] || [ -z ${backup_archive_name} ]
+then
+    echo Not enough options
+    exit 1
+fi 
 if [ ! -e "$backup_folder" ]
 then
 mkdir "$backup_folder"
 fi
-find "$input_folder" -type f -name "*.$extension" -exec cp --backup=numbered "{}" "$backup_folder" \;
+find "$input_folder" -type f -name "*.$extension" -exec cp -v "$backup_folder" \;
 tar -czf "$backup_archive_name" "$backup_folder"
 echo "done"
